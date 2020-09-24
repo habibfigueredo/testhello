4fc125bede423a472e1567923a872b2828a79ac9
habibfigueredo <h4bibfigueredo@hotmail.com>
2020-09-24 15:29:34 -0400

    Initial commit

---
+++ src/main/java/com/valaxy/helloworld/App.java
@@ -0,0 +1,13 @@
+package com.valaxy.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
